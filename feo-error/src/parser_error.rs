15130c565706162080a2a2a6cfecbd886eafba82
Matthew Pratt <matthew.pratt13@gmail.com>
2023-12-12 18:30:31 +0200

    fix errors

--- feo-error/src/parser_error.rs
+++ feo-error/src/parser_error.rs
@@ -2,5 +2,12 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum ParserErrorKind {
- 
+    #[error("unable to parse bool")]
+    ParseBoolError,
+    #[error("unable to parse char")]
+    ParseCharError,
+    #[error("unable to parse float")]
+    ParseFloatError,
+    #[error("unable to parse int")]
+    ParseIntError,
 }
