f1d5bedc4da4d4f724356cad1e4eac51a95cd792
Matthew Pratt <matthew.pratt13@gmail.com>
2023-12-12 17:07:06 +0200

    adjustments

--- feo-error/src/type_error.rs
+++ feo-error/src/type_error.rs
@@ -4,6 +4,7 @@ use feo_types::Span;
 
 #[derive(Debug, Error)]
 pub enum TypeErrorKind {
+    // TODO: remove these conversions - this crate should not know about feo_types
     #[error("unrecognized delimiter")]
     DelimiterError(#[from] feo_types::DelimiterError),
     #[error("span error")]
--- feo-parser/src/lexer.rs
+++ feo-parser/src/lexer.rs
@@ -1,4 +1,3 @@
-use std::iter::Peekable;
 use std::sync::Arc;
 
 use feo_error::LexErrorKind;
@@ -11,20 +10,26 @@ pub struct Lexer<'a> {
     pos: usize,
 }
 
-type CharReader<'a> = Peekable<Lexer<'a>>;
-
 impl Iterator for Lexer<'_> {
     type Item = char;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        let src = Arc::into_inner(Arc::clone(&self.src)).unwrap_or("");
+
+        if let Some(c) = src.chars().next() {
+            self.pos += 1;
+            Some(c)
+        } else {
+            self.pos = src.len();
+            None
+        }
     }
 }
 
 impl<'a> Lexer<'a> {
-    fn new(src: &'a str) -> Self {
+    fn new(data: &'a str) -> Self {
         Self {
-            src: Arc::new(src),
+            src: Arc::new(data),
             pos: 0,
         }
     }
@@ -32,17 +37,23 @@ impl<'a> Lexer<'a> {
     fn tokenize(&mut self) -> Result<TokenStream<TokenTree>, LexErrorKind> {
         let src = Arc::into_inner(Arc::clone(&self.src)).ok_or(LexErrorKind::SourceFileEmpty)?;
 
+        let char_reader = &mut Lexer {
+            src: Arc::clone(&self.src),
+            pos: self.pos,
+        }
+        .peekable();
+
         let mut tokens: Vec<Option<Token>> = Vec::new();
         let mut token_trees: Vec<Option<TokenTree>> = Vec::new();
 
-        let mut is_negative_number = false;
         let mut in_block_comment = false; // generic inline / multiline comment
+        let mut is_negative_number = false;
         let mut is_hexadecimal_int = false;
 
-        let mut open_delimiters: usize = 0;
+        let mut num_open_delimiters: usize = 0;
         let mut file_start_offset: usize = 0;
 
-        while let Some(c) = self.next() {
+        while let Some(c) = char_reader.next() {
             let start_index = self.pos;
 
             match c {
@@ -52,35 +63,36 @@ impl<'a> Lexer<'a> {
                     }
                     continue;
                 }
-                // _ if c == '*' && self.peek() == Some('/') => {
-                // if !in_block_comment {
-                // throw error
-                // } else {
-                // self.next(); // skip '*'
-                // self.next(); // skip '/'
-                // in_block_comment = false;
-                // }
-                // continue;
-                // }
+                _ if c == '*' && char_reader.peek() == Some(&'/') => {
+                    if !in_block_comment {
+                        // throw error
+                    } else {
+                        char_reader.next(); // skip '*'
+                        char_reader.next(); // skip '/'
+                        in_block_comment = false;
+                    }
+                    continue;
+                }
                 _ if c == '/' => {
-                    match self.next() {
+                    match char_reader.next() {
                         Some('/') => {
-                            self.next(); // skip second '/'
-                                         // if let Some('/') = self.peek() {
-                                         // self.next(); // skip third '/'
-                                         //              //   parse doc comment
-                                         // } else {
-                                         // //  parse newline / trailing comment
-                                         // continue;
-                                         // }
+                            char_reader.next();
+                            // skip second '/'
+                            if let Some('/') = char_reader.peek() {
+                                char_reader.next(); // skip third '/'
+                                               //   parse doc comment
+                            } else {
+                                //  parse newline / trailing comment
+                                continue;
+                            }
                             continue;
                         }
                         Some('*') => {
-                            self.next(); // skip '*'
+                            char_reader.next(); // skip '*'
                             in_block_comment = true;
                             // parse inline / multiline comment
-                            self.next(); // skip closing '*'
-                            self.next(); // skip closing '/'
+                            char_reader.next(); // skip closing '*'
+                            char_reader.next(); // skip closing '/'
                             in_block_comment = false;
                             continue;
                         }
@@ -93,38 +105,38 @@ impl<'a> Lexer<'a> {
                 }
 
                 '(' | '[' | '{' => {
-                    open_delimiters += 1;
+                    num_open_delimiters += 1;
                     // parse opening delimiter
                 }
 
                 ')' | ']' | '}' => {
                     // parse closing delimiter
-                    open_delimiters -= 1;
+                    num_open_delimiters -= 1;
                 }
 
                 '"' => {
-                    self.next(); // skip opening double quote
-                                 // parse string literal
-                    self.next(); // skip closing double quote
+                    char_reader.next(); // skip opening double quote
+                                   // parse string literal
+                    char_reader.next(); // skip closing double quote
                 }
                 '\'' => {
-                    self.next(); // skip opening single quote
-                                 // parse char literal
-                    self.next(); // skip opening single quote
+                    char_reader.next(); // skip opening single quote
+                                   // parse char literal
+                    char_reader.next(); // skip opening single quote
                 }
 
                 // handle negative numbers; do we allow for example "-.3" ?
                 // does `is_digit()` include floats?
-                // _ if c == '-' && self.peek().is_some_and(|c| c.is_digit(10 | 16)) => {
-                // is_negative_number = true;
-                // }
+                _ if c == '-' && char_reader.peek().is_some_and(|c| c.is_digit(10 | 16)) => {
+                    is_negative_number = true;
+                }
 
                 // account for hexadecimal prefix
-                // _ if c == '0' && self.peek() == Some('x') => {
-                // self.next(); // skip '0'
-                // self.next() // skip 'x'
-                // is_hexadecimal_digit = true;
-                // }
+                _ if c == '0' && char_reader.peek() == Some(&'x') => {
+                    char_reader.next(); // skip '0'
+                    char_reader.next(); // skip 'x'
+                    is_hexadecimal_int = true;
+                }
                 '0'..='9' | 'a'..='f' | 'A'..='F' => {
                     // parse digits
                     // continue;
@@ -138,7 +150,7 @@ impl<'a> Lexer<'a> {
             }
         }
 
-        if open_delimiters != 0 {
+        if num_open_delimiters != 0 {
             return Err(LexErrorKind::UnclosedDelimiters);
         }
 
