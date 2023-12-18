b18d92148898bc599fb193730634b57e65eee278
Matthew Pratt <matthew.pratt13@gmail.com>
2023-12-18 15:32:22 +0200

    add Display trait trait requirement

--- feo-parser/src/parse.rs
+++ feo-parser/src/parse.rs
@@ -1,10 +1,12 @@
+use std::fmt::Display;
+
 use feo_types::Primitive;
 
 use crate::{error::ParserError, lexer::Token};
 
 pub trait Parse<T>
 where
-    T: 'static + Primitive,
+    T: 'static + Primitive + Display,
 {
     fn parse(
         src: &str,
@@ -16,7 +18,7 @@ where
 
 pub trait ParseVec<T>
 where
-    T: 'static + Primitive,
+    T: 'static + Primitive + Display,
 {
     fn parse(
         src: &str,
