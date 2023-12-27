b11dcc49544cfa3f1c597479bf642aa12e3e304a
Matthew Pratt <matthew.pratt13@gmail.com>
2023-12-27 14:02:07 +0200

    add Position struct

--- feo-types/src/span.rs
+++ feo-types/src/span.rs
@@ -4,6 +4,31 @@ pub trait Spanned {
     fn span(&self) -> &Span;
 }
 
+pub struct Position<'a> {
+    input: &'a str,
+    pos: usize,
+}
+
+impl<'a> Position<'a> {
+    pub fn new(input: &'a str, pos: usize) -> Option<Position<'a>> {
+        input.get(pos..).map(|_| Position { input, pos })
+    }
+
+    #[inline]
+    pub fn line_col(&self) -> (usize, usize) {
+        if self.pos > self.input.len() {
+            panic!("Position out of bounds");
+        }
+
+        let slice = &self.input[..self.pos];
+        let lines = slice.split('\n').collect::<Vec<_>>();
+        let line_count = lines.len();
+        let last_line_len = lines.last().unwrap_or(&"").chars().count() + 1;
+
+        (line_count, last_line_len)
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct Span {
     src: Arc<String>,
