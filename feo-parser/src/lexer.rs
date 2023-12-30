440972d6393b9ad9ba1573d4f093fd25766ed07e
Matthew Pratt <matthew.pratt13@gmail.com>
2023-12-30 23:01:13 +0200

    rename vars

--- feo-parser/src/lexer.rs
+++ feo-parser/src/lexer.rs
@@ -18,7 +18,6 @@ use feo_error::{
     handler::{ErrorEmitted, Handler},
     lex_error::{LexError, LexErrorKind},
 };
-use feo_types::span::Span;
 
 #[allow(dead_code)]
 struct Lexer<'a> {
@@ -202,32 +201,6 @@ impl<'a> Lexer<'a> {
                             self.advance();
                         } else if c == ':' {
                             self.advance(); // skip ':'
-
-                            if self.peek_next() == Some(':') {
-                                self.advance(); // skip second ':'
-
-                                let span = Span::new(&self.input, start_pos, start_pos + buf.len());
-
-                                let prefix = Identifier::new(buf.to_string(), span);
-
-                                while let Some(c) = self.current_char() {
-                                    if c.is_alphanumeric() || c == '_' {
-                                        let mut component = String::new();
-                                        component.push(c);
-                                        self.advance();
-
-                                        while let Some(next_c) = self.current_char() {
-                                            if next_c.is_alphanumeric() || c == 
-
-
-                                        }
-
-
-                                    }
-                                }
-
-                                let mut path_components = vec![&buf];
-                            }
 
                             self.skip_whitespace();
                             let mut type_name = String::new();
@@ -703,9 +676,6 @@ mod tests {
         */
         /// doc comment
         
-
-        import crate::module::Struct;
-
         struct Foo {
             a: String // trailing comment,
             b: i32,
