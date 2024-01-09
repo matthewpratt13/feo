e9737a433e3c0374f39ab584e004a02ee2615a7c
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-09 23:58:43 +0200

    update Expressions

--- feo-ast/src/expression.rs
+++ feo-ast/src/expression.rs
@@ -5,43 +5,58 @@ mod conditional_expr;
 mod iterator_expr;
 mod literal_expr;
 mod operator_expr;
+mod tuple_expr;
 
 mod path_expr;
+
+use crate::keyword::KeywordKind;
+
 pub use self::path_expr::PathExpr;
+use self::{
+    array_expr::{ArrayExpr, IndexExpr},
+    block_expr::BlockExpr,
+    call_expr::{FunctionCallExpr, MethodCallExpr},
+    conditional_expr::{IfExpr, MatchExpr},
+    iterator_expr::LoopExpr,
+    literal_expr::LiteralExpr,
+    operator_expr::OperatorExpr,
+    struct_expr::StructExpr,
+    tuple_expr::{TupleExpr, TupleIndexingExpr},
+};
 
 mod struct_expr;
 
 pub enum Expression {
-    ExprWithoutBlock,
-    ExprWithBlock,
+    WithoutBlock(ExprWithoutBlock),
+    WithBlock(ExprWithBlock),
 }
 
 pub enum ExprWithoutBlock {
-    Attribute,
-    ArrayExpr,
-    FunctionCallExpr,
-    MethodCallExpr,
-    BreakExpr,
-    ContinueExpr,
-    FieldAccessExpr,
-    GroupedExpr,
-    IndexExpr,
-    LiteralExpr,
-    OperatorExpr,
-    PathExpr,
-    StructExpr,
-    TupleExpr,
-    TupleIndexingExpr,
-    ReturnExpr,
-    UnderscoreExpr,
+    Attr(Attribute),
+    Array(ArrayExpr),
+    FunctionCall(FunctionCallExpr),
+    MethodCall(MethodCallExpr),
+    Break(KeywordKind),
+    Continue(KeywordKind),
+    FieldAccess(FieldAccessExpr),
+    Grouped(GroupedExpr),
+    Index(IndexExpr),
+    Literal(LiteralExpr),
+    Operator(OperatorExpr),
+    Path(PathExpr),
+    Struct(StructExpr),
+    Tuple(TupleExpr),
+    TupleIndexing(TupleIndexingExpr),
+    Return(ReturnExpr),
+    Underscore(UnderscoreExpr),
 }
 
 pub enum ExprWithBlock {
-    Attribute,
-    BlockExpr,
-    IfExpr,
-    LoopExpr,
-    MatchExpr,
+    Attr(Attribute),
+    Block(BlockExpr),
+    IfExpr(IfExpr),
+    LoopExpr(LoopExpr),
+    MatchExpr(MatchExpr),
 }
 
 pub struct Attribute {}
