fd2043412927fb1ac15130c08f78ccf4e0ac6f86
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-10 18:08:06 +0200

    add array expression

--- feo-ast/src/expression/array_expr.rs
+++ feo-ast/src/expression/array_expr.rs
@@ -1,5 +1,31 @@
-pub struct ArrayExpr {}
+use crate::{
+    delimiter::{DelimKind, DelimOrientation},
+    punctuation::PuncKind,
+};
 
-pub struct ArrayElements {}
+use super::Expression;
 
-pub struct IndexExpr {}
+pub struct ArrayExpr {
+    open_bracket: (DelimKind, DelimOrientation),
+    array_elements: Box<ArrayElements>,
+    close_bracket: (DelimKind, DelimOrientation),
+}
+
+pub struct ArrayElements {
+    opening_expression: Expression,
+    elements: Vec<(PuncKind, Expression)>,
+    trailing_comma: Option<PuncKind>,
+}
+
+pub struct ArrayElementsFixedSize {
+    element_type: Expression,
+    semicolon: PuncKind,
+    num_elements: usize,
+}
+
+pub struct IndexExpr {
+    item: Box<Expression>,
+    open_bracket: PuncKind,
+    index: usize,
+    close_bracket: PuncKind,
+}
