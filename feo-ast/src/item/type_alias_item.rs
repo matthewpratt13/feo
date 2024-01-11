7601c721c12e0536cafa388f25df549f51c0c6b9
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-11 18:58:04 +0200

    add TypeAliasItem

--- feo-ast/src/item/type_alias_item.rs
+++ feo-ast/src/item/type_alias_item.rs
@@ -1,1 +1,8 @@
-pub struct TypeAliasItem {}
+use crate::{identifier::Identifier, keyword::KeywordKind, punctuation::PuncKind, ty::Type};
+
+pub struct TypeAliasItem {
+    kw_type: KeywordKind,
+    name: Identifier,
+    value_opt: (PuncKind, Type),
+    semicolon: PuncKind,
+}
