ddcf03cac8af0c1bc30ae388b4791b593ba7337a
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-11 18:12:31 +0200

    add Struct StructItem

--- feo-ast/src/item/struct_item.rs
+++ feo-ast/src/item/struct_item.rs
@@ -1,9 +1,22 @@
+use crate::{
+    delimiter::{DelimKind, DelimOrientation},
+    identifier::Identifier,
+    keyword::KeywordKind,
+    punctuation::PuncKind,
+};
+
 pub enum StructItem {
     Struct(Struct),
     TupleStruct(TupleStruct),
 }
 
-pub struct Struct {}
+pub struct Struct {
+    kw_struct: KeywordKind,
+    name: Identifier,
+    open_brace: (DelimKind, DelimOrientation),
+    struct_item_fields_opt: Option<StructItemFields>,
+    close_brace: (DelimKind, DelimOrientation),
+}
 
 pub struct StructItemFields {}
 
