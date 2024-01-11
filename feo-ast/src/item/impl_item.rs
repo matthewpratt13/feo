f9179da14f0884bcf5ab24d051f48a42ebac27b6
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-11 20:30:02 +0200

    add ImplItem

--- feo-ast/src/item/impl_item.rs
+++ feo-ast/src/item/impl_item.rs
@@ -1,8 +1,31 @@
+use crate::{
+    delimiter::{DelimKind, DelimOrientation},
+    keyword::KeywordKind,
+    ty::Type,
+};
+
+use super::associated_item::AssociatedItem;
+
 pub enum ImplItem {
     Inherent(InherentImpl),
     Trait(TraitImpl),
 }
 
-pub struct InherentImpl {}
+pub struct InherentImpl {
+    kw_impl: KeywordKind,
+    object_type: Type,
+    open_brace: (DelimKind, DelimOrientation),
+    associated_items: Vec<AssociatedItem>,
+    close_brace: (DelimKind, DelimOrientation),
+}
 
-pub struct TraitImpl {}
+pub struct TraitImpl {
+    kw_unsafe_opt: Option<KeywordKind>,
+    kw_trait: KeywordKind,
+    trait_type: Type,
+    kw_for: KeywordKind,
+    object_type: Type,
+    open_brace: (DelimKind, DelimOrientation),
+    associated_items: Vec<AssociatedItem>,
+    close_brace: (DelimKind, DelimOrientation),
+}
