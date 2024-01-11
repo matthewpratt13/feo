022241a38d03f956412fe5b2bb91eb2ed4bf2cd8
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-11 22:57:09 +0200

    add TraitItem

--- feo-ast/src/item/trait_item.rs
+++ feo-ast/src/item/trait_item.rs
@@ -1,2 +1,13 @@
-pub struct TraitItem {}
+use crate::{identifier::Identifier, keyword::KeywordKind};
 
+use super::associated_item::AssociatedItem;
+use super::Brace;
+
+pub struct TraitItem {
+    kw_unsafe_opt: Option<KeywordKind>,
+    kw_impl: KeywordKind,
+    name: Identifier,
+    open_brace: Brace,
+    associated_items: Vec<AssociatedItem>,
+    close_brace: Brace,
+}
