c1c039649e396d62359ee5f441889d404302406d
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-11 19:12:45 +0200

    add ConstantItem and StaticItem

--- feo-ast/src/item/constant_item.rs
+++ feo-ast/src/item/constant_item.rs
@@ -1,3 +1,23 @@
-pub struct ConstantItem {}
+use crate::{
+    expression::Expression, identifier::Identifier, keyword::KeywordKind, punctuation::PuncKind,
+    ty::Type,
+};
 
-pub struct StaticItem {}
+pub struct ConstantItem {
+    kw_const: KeywordKind,
+    name: Identifier,
+    colon: PuncKind,
+    item_type: Type,
+    assignment_opt: Option<(PuncKind, Box<Expression>)>,
+    semicolon: PuncKind,
+}
+
+pub struct StaticItem {
+    kw_static: KeywordKind,
+    kw_mut_opt: Option<KeywordKind>,
+    name: Identifier,
+    colon: PuncKind,
+    item_type: Type,
+    assignment_opt: Option<(PuncKind, Box<Expression>)>,
+    semicolon: PuncKind,
+}
