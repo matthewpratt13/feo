b252b1312113dd5129cb3e5a55a8eacee23a511a
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-11 19:25:47 +0200

    add EnumItem

--- feo-ast/src/item/enum_item.rs
+++ feo-ast/src/item/enum_item.rs
@@ -1,7 +1,39 @@
-pub struct EnumItem {}
+use crate::{
+    delimiter::{DelimKind, DelimOrientation},
+    expression::Attribute,
+    identifier::Identifier,
+    keyword::KeywordKind,
+    punctuation::PuncKind,
+};
 
-pub struct EnumItems {}
+use super::visibility::Visibility;
 
-pub struct EnumItemTuple {}
+pub enum EnumVariantType {
+    Struct(EnumItemStruct),
+    Tuple(EnumItemTuple),
+}
+
+pub struct EnumItem {
+    kw_enum: KeywordKind,
+    name: Identifier,
+    open_brace: (DelimKind, DelimOrientation),
+    enum_variants_opt: Option<EnumVariants>,
+    close_brace: (DelimKind, DelimOrientation),
+}
+
+pub struct EnumVariants {
+    first_variant: EnumVariant,
+    subsequent_variants: Vec<(PuncKind, EnumVariant)>,
+    trailing_comma_opt: Option<PuncKind>,
+}
+
+pub struct EnumVariant {
+    attributes: Vec<Attribute>,
+    visibility_opt: Option<Visibility>,
+    name: Identifier,
+    variant_type_opt: Option<EnumVariantType>,
+}
 
 pub struct EnumItemStruct {}
+
+pub struct EnumItemTuple {}
