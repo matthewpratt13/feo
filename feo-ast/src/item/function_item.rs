6505bc379566b72cf537daca39a18d3ce91a7a67
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-11 19:52:53 +0200

    add FuncQualitfier

--- feo-ast/src/item/function_item.rs
+++ feo-ast/src/item/function_item.rs
@@ -7,8 +7,14 @@ use crate::{
     ty::Type,
 };
 
+pub enum FuncQualifier {
+    Const(KeywordKind),
+    Unsafe(KeywordKind),
+    Extern(KeywordKind),
+}
+
 pub struct FunctionItem {
-    func_qualifiers: FuncQualifiers,
+    func_qualifiers_opt: Option<Vec<FuncQualifier>>,
     kw_func: KeywordKind,
     name: Identifier,
     open_parenthesis: (DelimKind, DelimOrientation),
@@ -19,7 +25,7 @@ pub struct FunctionItem {
 }
 
 pub struct FunctionSignature {
-    func_qualifiers: FuncQualifiers,
+    func_qualifiers_opt: Option<FuncQualifier>,
     kw_func: KeywordKind,
     name: Identifier,
     open_parenthesis: (DelimKind, DelimOrientation),
@@ -28,8 +34,6 @@ pub struct FunctionSignature {
     return_type_opt: Option<(PuncKind, Type)>,
     semicolon: PuncKind,
 }
-
-pub struct FuncQualifiers {}
 
 pub struct FuncParams {}
 
