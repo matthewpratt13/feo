e8a7b18512114bf60e720eee73969ea9a3f1dea7
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-11 16:21:23 +0200

    change PuncKind to Delimiter

--- feo-ast/src/expression/block_expr.rs
+++ feo-ast/src/expression/block_expr.rs
@@ -1,15 +1,18 @@
-use crate::{punctuation::PuncKind, statement::{Statement, StatementWithExpr}};
+use crate::{
+    delimiter::{DelimKind, DelimOrientation},
+    statement::{Statement, StatementWithExpr},
+};
 
 use super::ExprWithoutBlock;
-
-pub struct BlockExpr {
-    open_brace: PuncKind,
-    statements: Statements,
-    close_brace: PuncKind,
-}
 
 pub enum Statements {
     Statement(Box<Statement>),
     StatementWithExpr(Box<StatementWithExpr>),
     ExprWithoutBlock(ExprWithoutBlock),
+}
+
+pub struct BlockExpr {
+    open_brace: (DelimKind, DelimOrientation),
+    statements: Statements,
+    close_brace: (DelimKind, DelimOrientation),
 }
