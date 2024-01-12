d84b825e6e9577a62424b38aca6e0104043708ef
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-12 15:35:30 +0200

    rename struct

--- feo-ast/src/pattern/slice_patt.rs
+++ feo-ast/src/pattern/slice_patt.rs
@@ -4,11 +4,11 @@ use super::Pattern;
 
 pub struct SlicePatt {
     open_bracket: Bracket,
-    slice_pattern_items_opt: Option<SlicePattItems>,
+    slice_pattern_elements_opt: Option<SlicePattElements>,
     close_bracket: Bracket,
 }
 
-pub struct SlicePattItems {
+pub struct SlicePattElements {
     first_pattern: Box<Pattern>,
     subsequent_patterns: Vec<(Comma, Pattern)>,
     trailing_comma_opt: Option<Comma>,
