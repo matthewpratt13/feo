05f345c6234fc7c3d665331f95f962091730d450
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-16 22:27:14 +0200

    implement Spanned for ArrayExpr

--- feo-ast/src/expression/array_expr.rs
+++ feo-ast/src/expression/array_expr.rs
@@ -1,3 +1,5 @@
+use feo_types::span::{Span, Spanned};
+
 use crate::type_utils::{Bracket, Comma, Semicolon};
 
 use super::{ExprWithoutBlock, Expression};
@@ -11,6 +13,18 @@ pub struct ArrayExpr {
 impl Expression for ArrayExpr {}
 
 impl<E> ExprWithoutBlock<E> for ArrayExpr where E: Expression {}
+
+impl Spanned for ArrayExpr {
+    fn span(&self) -> Span {
+        let start_pos = self.open_bracket.span().start();
+        let end_pos = self.close_bracket.span().end();
+        let source = self.open_bracket.span().source();
+
+        let span = Span::new(source.as_str(), start_pos, end_pos);
+
+        span
+    }
+}
 
 pub struct ArrayElements {
     first_element: Box<dyn Expression>,
