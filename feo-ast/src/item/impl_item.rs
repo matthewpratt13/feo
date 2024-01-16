acc87c8b5faaad621d87a5dde3ee9f53ced37df1
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-16 16:17:01 +0200

    update ImplItem

--- feo-ast/src/item/impl_item.rs
+++ feo-ast/src/item/impl_item.rs
@@ -1,12 +1,16 @@
-use crate::{keyword::KeywordKind, path::SimplePath, ty::Type, type_utils::Brace};
+use crate::{
+    expression::Attribute, keyword::KeywordKind, path::SimplePath, ty::Type, type_utils::Brace,
+};
 
-use super::{AssociatedItem, VisibilityKind};
+use super::{AssociatedItem, VisibilityKind, WhereClause};
 
 pub struct InherentImpl<T> {
     visibility_opt: Option<VisibilityKind>,
     kw_impl: KeywordKind,
     object_type: Box<dyn Type>,
+    where_clause_opt: Option<WhereClause>,
     open_brace: Brace,
+    attributes: Vec<Attribute>,
     associated_items: Vec<Box<dyn AssociatedItem<T>>>,
     close_brace: Brace,
 }
@@ -18,7 +22,9 @@ pub struct TraitImpl<T> {
     trait_path: SimplePath,
     kw_for: KeywordKind,
     object_type: Box<dyn Type>,
+    where_clause_opt: Option<WhereClause>,
     open_brace: Brace,
+    attributes: Vec<Attribute>,
     associated_items: Vec<Box<dyn AssociatedItem<T>>>,
     close_brace: Brace,
 }
