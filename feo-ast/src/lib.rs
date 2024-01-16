dee1c771114b8aa948d0a55eb5ddc6f5840d93bb
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-16 21:43:12 +0200

    edit type_utils

--- feo-ast/src/lib.rs
+++ feo-ast/src/lib.rs
@@ -16,32 +16,32 @@ pub mod ty;
 pub mod type_annotation;
 
 mod type_utils {
-    use crate::delimiter::{DelimKind, DelimOrientation};
-    use crate::punctuation::PuncKind;
+    use crate::delimiter::Delimiter;
+    use crate::punctuation::Punctuation;
 
-    pub type Asterisk = PuncKind;
-    pub type Bang = PuncKind;
-    pub type Colon = PuncKind;
-    pub type Comma = PuncKind;
-    pub type DblColon = PuncKind;
-    pub type DblDot = PuncKind;
-    pub type DblPipe = PuncKind;
-    pub type Dot = PuncKind;
-    pub type DotDotEquals = PuncKind;
-    pub type Equals = PuncKind;
-    pub type FatArrow = PuncKind;
-    pub type HashSign = PuncKind;
-    pub type HashBang = PuncKind;
-    pub type OpArithmeticOrLogical = PuncKind;
-    pub type OpBool = PuncKind;
-    pub type OpComparison = PuncKind;
-    pub type Pipe = PuncKind;
-    pub type Plus = PuncKind;
-    pub type QuestionMark = PuncKind;
-    pub type Semicolon = PuncKind;
-    pub type ThinArrow = PuncKind;
+    pub type Asterisk = Punctuation;
+    pub type Bang = Punctuation;
+    pub type Colon = Punctuation;
+    pub type Comma = Punctuation;
+    pub type DblColon = Punctuation;
+    pub type DblDot = Punctuation;
+    pub type DblPipe = Punctuation;
+    pub type Dot = Punctuation;
+    pub type DotDotEquals = Punctuation;
+    pub type Equals = Punctuation;
+    pub type FatArrow = Punctuation;
+    pub type HashSign = Punctuation;
+    pub type HashBang = Punctuation;
+    pub type OpArithmeticOrLogical = Punctuation;
+    pub type OpBool = Punctuation;
+    pub type OpComparison = Punctuation;
+    pub type Pipe = Punctuation;
+    pub type Plus = Punctuation;
+    pub type QuestionMark = Punctuation;
+    pub type Semicolon = Punctuation;
+    pub type ThinArrow = Punctuation;
 
-    pub type Brace = (DelimKind, DelimOrientation);
-    pub type Bracket = (DelimKind, DelimOrientation);
-    pub type Parenthesis = (DelimKind, DelimOrientation);
+    pub type Brace = Delimiter;
+    pub type Bracket = Delimiter;
+    pub type Parenthesis = Delimiter;
 }
