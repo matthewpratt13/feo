abfff4ac377fe6d3015fcf6a80e51dbb73fbe257
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-16 23:21:46 +0200

    implemebnt Spanned for statements

--- feo-ast/src/statement.rs
+++ feo-ast/src/statement.rs
@@ -1,9 +1,11 @@
 #![allow(dead_code)]
 
+use feo_types::span::{Span, Spanned};
+
 use crate::{
     expression::{ExprWithoutBlock, Expression},
     identifier::Identifier,
-    keyword::KeywordKind,
+    keyword::Keyword,
     type_annotation::TypeAnnKind,
     type_utils::{Colon, Equals, Semicolon},
 };
@@ -17,9 +19,21 @@ pub struct ExprStatement<T> {
 
 impl<T> Statement for ExprStatement<T> {}
 
+impl<T> Spanned for ExprStatement<T> {
+    fn span(&self) -> Span {
+        let start_pos = todo!();
+        let end_pos = self.semicolon.span().end();
+        let source = self.semicolon.span().source();
+
+        let span = Span::new(source.as_str(), start_pos, end_pos);
+
+        span
+    }
+}
+
 pub struct LetStatement {
-    kw_let: KeywordKind,
-    kw_mut_opt: Option<KeywordKind>,
+    kw_let: Keyword,
+    kw_mut_opt: Option<Keyword>,
     identifier: Identifier,
     type_ann_opt: Option<(Colon, TypeAnnKind)>,
     equals: Equals,
@@ -29,9 +43,33 @@ pub struct LetStatement {
 
 impl Statement for LetStatement {}
 
+impl Spanned for LetStatement {
+    fn span(&self) -> Span {
+        let start_pos = self.kw_let.span().start();
+        let end_pos = self.semicolon.span().end();
+        let source = self.kw_let.span().source();
+
+        let span = Span::new(source.as_str(), start_pos, end_pos);
+
+        span
+    }
+}
+
 pub struct StatementWithExpr<T> {
     statement: Box<dyn Statement>,
     expr_without_block: Box<dyn ExprWithoutBlock<T>>,
 }
 
 impl<T> Statement for StatementWithExpr<T> {}
+
+// impl<T> Spanned for StatementWithExpr<T> {
+//     fn span(&self) -> Span {
+//         let start_pos = todo!();
+//         let end_pos = todo!();
+//         let source = todo!();
+
+//         let span = Span::new(source.as_str(), start_pos, end_pos);
+
+//         span
+//     }
+// }
