7c10a0d91d8c6fb7fc49151092ac1d1ed5f9e876
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-16 22:49:22 +0200

    implement Spanned for IfExpr

--- feo-ast/src/expression/conditional_expr.rs
+++ feo-ast/src/expression/conditional_expr.rs
@@ -1,5 +1,7 @@
+use feo_types::span::{Span, Spanned};
+
 use crate::{
-    keyword::KeywordKind,
+    keyword::Keyword,
     pattern::Pattern,
     type_utils::{Brace, Comma, FatArrow},
 };
@@ -7,11 +9,13 @@ use crate::{
 use super::{BlockExpr, ConditionalExpr, ExprWithBlock, Expression, InnerAttr, OuterAttr};
 
 pub struct IfExpr<T, U> {
-    kw_if: KeywordKind,
+    kw_if: Keyword,
     condition: Box<dyn Expression>, // cannot be a struct expression
     block: BlockExpr<T, U>,
-    else_if_block_opt: Option<(KeywordKind, Box<IfExpr<T, U>>)>,
-    else_block_opt: Option<(KeywordKind, BlockExpr<T, U>)>,
+
+    // TODO: should these rather be enum variants?
+    else_if_block_opt: Option<(Keyword, Box<IfExpr<T, U>>)>,
+    else_block_opt: Option<(Keyword, BlockExpr<T, U>)>,
 }
 
 impl<T, U> Expression for IfExpr<T, U> {}
@@ -20,8 +24,30 @@ impl<T, U, C> ConditionalExpr<C> for IfExpr<T, U> where C: Expression {}
 
 impl<T, U, E> ExprWithBlock<E> for IfExpr<T, U> where E: Expression {}
 
+impl<T, U> Spanned for IfExpr<T, U> {
+    fn span(&self) -> Span {
+        let start_pos = self.kw_if.span().start();
+        let temp_end = self.block.span().end();
+
+        let end_pos = match &self.else_if_block_opt {
+            Some(s) => match &self.else_block_opt {
+                Some(t) => t.1.span().end(),
+                None => s.1.span().end(),
+            },
+
+            None => temp_end,
+        };
+
+        let source = self.kw_if.span().source();
+
+        let span = Span::new(source.as_str(), start_pos, end_pos);
+
+        span
+    }
+}
+
 pub struct MatchExpr {
-    kw_match: KeywordKind,
+    kw_match: Keyword,
     scrutinee: Box<dyn Expression>, // cannot be a struct expression
     open_brace: Brace,
     attributes: Vec<InnerAttr>,
@@ -47,6 +73,6 @@ pub struct MatchArm {
 }
 
 pub struct MatchArmGuard {
-    kw_if: KeywordKind,
+    kw_if: Keyword,
     condition: Box<dyn Expression>,
 }
