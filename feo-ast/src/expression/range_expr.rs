2c5fd404e70704f16a0b20587e60bba3db66ff6a
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-16 23:08:51 +0200

    implement Spanned for range expressions

--- feo-ast/src/expression/range_expr.rs
+++ feo-ast/src/expression/range_expr.rs
@@ -1,3 +1,5 @@
+use feo_types::span::{Span, Spanned};
+
 use crate::type_utils::{DblDot, DotDotEquals};
 
 use super::{ExprWithoutBlock, Expression, RangeExpr};
@@ -14,6 +16,18 @@ impl<E> ExprWithoutBlock<E> for RangeFromToExpr where E: Expression {}
 
 impl<R> RangeExpr<R> for RangeFromToExpr where R: Expression {}
 
+impl Spanned for RangeFromToExpr {
+    fn span(&self) -> Span {
+        let start_pos = todo!();
+        let end_pos = todo!();
+        let source = self.dbl_dot.span().source();
+
+        let span = Span::new(source.as_str(), start_pos, end_pos);
+
+        span
+    }
+}
+
 pub struct RangeFromExpr {
     from_expression: Box<dyn Expression>,
     dbl_dot: DblDot,
@@ -25,6 +39,18 @@ impl<E> ExprWithoutBlock<E> for RangeFromExpr where E: Expression {}
 
 impl<R> RangeExpr<R> for RangeFromExpr where R: Expression {}
 
+impl Spanned for RangeFromExpr {
+    fn span(&self) -> Span {
+        let start_pos = todo!();
+        let end_pos = self.dbl_dot.span().end();
+        let source = self.dbl_dot.span().source();
+
+        let span = Span::new(source.as_str(), start_pos, end_pos);
+
+        span
+    }
+}
+
 pub struct RangeToExpr {
     dbl_dot: DblDot,
     to_expression: Box<dyn Expression>,
@@ -35,6 +61,18 @@ impl Expression for RangeToExpr {}
 impl<E> ExprWithoutBlock<E> for RangeToExpr where E: Expression {}
 
 impl<R> RangeExpr<R> for RangeToExpr where R: Expression {}
+
+impl Spanned for RangeToExpr {
+    fn span(&self) -> Span {
+        let start_pos = self.dbl_dot.span().start();
+        let end_pos = todo!();
+        let source = self.dbl_dot.span().source();
+
+        let span = Span::new(source.as_str(), start_pos, end_pos);
+
+        span
+    }
+}
 
 pub struct RangeInclusiveExpr {
     from_expression: Box<dyn Expression>,
@@ -48,6 +86,18 @@ impl<E> ExprWithoutBlock<E> for RangeInclusiveExpr where E: Expression {}
 
 impl<R> RangeExpr<R> for RangeInclusiveExpr where R: Expression {}
 
+impl Spanned for RangeInclusiveExpr {
+    fn span(&self) -> Span {
+        let start_pos = todo!()
+        let end_pos = todo!();
+        let source = self.dot_dot_equals.span().source();
+
+        let span = Span::new(source.as_str(), start_pos, end_pos);
+
+        span
+    }
+}
+
 pub struct RangeToInclusiveExpr {
     dot_dot_equals: DotDotEquals,
     to_expression_incl: Box<dyn Expression>,
@@ -58,3 +108,15 @@ impl Expression for RangeToInclusiveExpr {}
 impl<E> ExprWithoutBlock<E> for RangeToInclusiveExpr where E: Expression {}
 
 impl<R> RangeExpr<R> for RangeToInclusiveExpr where R: Expression {}
+
+impl Spanned for RangeToInclusiveExpr {
+    fn span(&self) -> Span {
+        let start_pos = self.dot_dot_equals.span().start();
+        let end_pos = todo!();
+        let source = self.dot_dot_equals.span().source();
+
+        let span = Span::new(source.as_str(), start_pos, end_pos);
+
+        span
+    }
+}
