9ca1ef40c7f53a3aaacd50a0ff533dba73e0e657
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-17 14:31:26 +0200

    implement Spanned for IdentifierPatt

--- feo-ast/src/pattern.rs
+++ feo-ast/src/pattern.rs
@@ -91,15 +91,36 @@ mod grouped_pattern {
 }
 
 mod identifier_patt {
-    use crate::{identifier::Identifier, keyword::KeywordKind};
+    use feo_types::span::{Span, Spanned};
+
+    use crate::{identifier::Identifier, keyword::Keyword};
 
     use super::Pattern;
 
     pub struct IdentifierPatt {
-        kw_ref_opt: Option<KeywordKind>,
-        kw_mut_opt: Option<KeywordKind>,
+        kw_ref_opt: Option<Keyword>,
+        kw_mut_opt: Option<Keyword>,
         name: Identifier,
     }
 
     impl Pattern for IdentifierPatt {}
+
+    impl Spanned for IdentifierPatt {
+        fn span(&self) -> Span {
+            let start_pos = match &self.kw_ref_opt {
+                Some(kwr) => match &self.kw_mut_opt {
+                    Some(kwm) => kwm.span().start(),
+                    None => kwr.span().start(),
+                },
+                None => self.name.span().start(),
+            };
+
+            let end_pos = self.name.span().end();
+            let source = self.name.span().source();
+
+            let span = Span::new(source.as_str(), start_pos, end_pos);
+
+            span
+        }
+    }
 }
