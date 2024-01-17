af8c798819dce7d970fffceb28770f7b49776299
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-17 16:17:42 +0200

    implement Spanned for TypeAliasItem

--- feo-ast/src/item/type_alias_item.rs
+++ feo-ast/src/item/type_alias_item.rs
@@ -1,7 +1,9 @@
+use feo_types::span::{Span, Spanned};
+
 use crate::{
     expression::OuterAttr,
     identifier::Identifier,
-    keyword::KeywordKind,
+    keyword::Keyword,
     ty::Type,
     type_utils::{Colon, Equals, Semicolon},
 };
@@ -11,7 +13,7 @@ use super::{AssociatedItem, Item, TypeParamBounds, VisibilityKind, WhereClause};
 pub struct TypeAliasItem {
     attributes: Vec<OuterAttr>,
     visibility_opt: Option<VisibilityKind>,
-    kw_type: KeywordKind,
+    kw_type: Keyword,
     name: Identifier,
     type_param_bounds_opt: Option<(Colon, TypeParamBounds)>,
     where_clause_opt: Option<WhereClause>,
@@ -22,3 +24,22 @@ pub struct TypeAliasItem {
 impl Item for TypeAliasItem {}
 
 impl<A> AssociatedItem<A> for TypeAliasItem where A: Item {}
+
+impl Spanned for TypeAliasItem {
+    fn span(&self) -> Span {
+        let start_pos = match self.attributes.first() {
+            Some(a) => a.span().start(),
+            None => match &self.visibility_opt {
+                Some(v) => v.span().start(),
+                None => self.kw_type.span().start(),
+            },
+        };
+
+        let end_pos = self.semicolon.span().end();
+        let source = self.name.span().source();
+
+        let span = Span::new(source.as_str(), start_pos, end_pos);
+
+        span
+    }
+}
