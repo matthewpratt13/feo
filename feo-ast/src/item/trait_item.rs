b812fb86f3cf4a787f3824e40b4ad327a4bb4757
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-17 16:14:05 +0200

    implement Spanned for TraitItem

--- feo-ast/src/item/trait_item.rs
+++ feo-ast/src/item/trait_item.rs
@@ -1,7 +1,9 @@
+use feo_types::span::{Span, Spanned};
+
 use crate::{
     expression::InnerAttr,
     identifier::Identifier,
-    keyword::KeywordKind,
+    keyword::Keyword,
     program::LibraryItem,
     type_utils::{Brace, Colon},
 };
@@ -10,8 +12,8 @@ use super::{AssociatedItem, Item, TypeParamBounds, VisibilityKind, WhereClause};
 
 pub struct TraitItem<T> {
     visibility_opt: Option<VisibilityKind>,
-    kw_unsafe_opt: Option<KeywordKind>,
-    kw_impl: KeywordKind,
+    kw_unsafe_opt: Option<Keyword>,
+    kw_impl: Keyword,
     name: Identifier,
     type_param_bounds_opt: Option<(Colon, Option<TypeParamBounds>)>,
     where_clause_opt: Option<WhereClause>,
@@ -24,3 +26,22 @@ pub struct TraitItem<T> {
 impl<T> Item for TraitItem<T> {}
 
 impl<T, L> LibraryItem<L> for TraitItem<T> where L: Item {}
+
+impl<T> Spanned for TraitItem<T> {
+    fn span(&self) -> Span {
+        let start_pos = match &self.visibility_opt {
+            Some(v) => v.span().start(),
+            None => match &self.kw_unsafe_opt {
+                Some(k) => k.span().start(),
+                None => self.kw_impl.span().start(),
+            },
+        };
+
+        let end_pos = self.name.span().end();
+        let source = self.name.span().source();
+
+        let span = Span::new(source.as_str(), start_pos, end_pos);
+
+        span
+    }
+}
