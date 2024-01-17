9d724e2b58846dd9c6511ce0fdf539b1184846d4
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-17 15:24:14 +0200

    implement Spanned for ExternCrateItem

--- feo-ast/src/item/extern_crate_item.rs
+++ feo-ast/src/item/extern_crate_item.rs
@@ -1,18 +1,20 @@
+use feo_types::span::{Span, Spanned};
+
 use crate::{
-    identifier::Identifier, keyword::KeywordKind, program::LibraryItem, type_utils::Semicolon,
+    identifier::Identifier, keyword::Keyword, program::LibraryItem, type_utils::Semicolon,
 };
 
 use super::{AsClause, Item, VisibilityKind};
 
 pub enum CrateRefKind {
     Identifier(Identifier),
-    KwSelf(KeywordKind),
+    KwSelf(Keyword),
 }
 
 pub struct ExternCrateItem {
     visibility_opt: Option<VisibilityKind>,
-    kw_extern: KeywordKind,
-    kw_crate: KeywordKind,
+    kw_extern: Keyword,
+    kw_crate: Keyword,
     crate_ref: CrateRefKind,
     as_clause_opt: Option<AsClause>,
     semicolon: Semicolon,
@@ -21,3 +23,20 @@ pub struct ExternCrateItem {
 impl Item for ExternCrateItem {}
 
 impl<L> LibraryItem<L> for ExternCrateItem where L: Item {}
+
+impl Spanned for ExternCrateItem {
+    fn span(&self) -> Span {
+        let start_pos = if let Some(vk) = &self.visibility_opt {
+            vk.span().start()
+        } else {
+            self.semicolon.span().start()
+        };
+
+        let end_pos = self.semicolon.span().end();
+        let source = self.kw_extern.span().source();
+
+        let span = Span::new(source.as_str(), start_pos, end_pos);
+
+        span
+    }
+}
