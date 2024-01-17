e6b626caeb5a062514ea0bb72ac86891f29b08eb
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-17 15:15:00 +0200

    implement Spanned for ConstantItem

--- feo-ast/src/item/constant_item.rs
+++ feo-ast/src/item/constant_item.rs
@@ -1,7 +1,9 @@
+use feo_types::span::{Span, Spanned};
+
 use crate::{
     expression::{Expression, OuterAttr},
     identifier::Identifier,
-    keyword::KeywordKind,
+    keyword::Keyword,
     ty::Type,
     type_utils::{Colon, Equals, Semicolon},
 };
@@ -11,7 +13,7 @@ use super::{AssociatedItem, Item, VisibilityKind};
 pub struct ConstantItem {
     attributes: Vec<OuterAttr>,
     visibility_opt: Option<VisibilityKind>,
-    kw_const: KeywordKind,
+    kw_const: Keyword,
     name: Identifier,
     colon: Colon,
     item_type: Box<dyn Type>,
@@ -23,10 +25,29 @@ impl Item for ConstantItem {}
 
 impl<A> AssociatedItem<A> for ConstantItem where A: Item {}
 
+impl Spanned for ConstantItem {
+    fn span(&self) -> Span {
+        let start_pos = match self.attributes.last() {
+            Some(oa) => match &self.visibility_opt {
+                Some(vk) => vk.span().start(),
+                None => oa.span().start(),
+            },
+            None => self.kw_const.span().start(),
+        };
+
+        let end_pos = self.semicolon.span().end();
+        let source = self.name.span().source();
+
+        let span = Span::new(source.as_str(), start_pos, end_pos);
+
+        span
+    }
+}
+
 pub struct StaticItem {
     visibility_opt: Option<VisibilityKind>,
-    kw_static: KeywordKind,
-    kw_mut_opt: Option<KeywordKind>,
+    kw_static: Keyword,
+    kw_mut_opt: Option<Keyword>,
     name: Identifier,
     colon: Colon,
     item_type: Box<dyn Type>,
