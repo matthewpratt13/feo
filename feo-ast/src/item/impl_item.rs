359a39627f04b4d8762a56e99ec9955153276fee
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-17 15:55:09 +0200

    implement Spanned for InherentImpl

--- feo-ast/src/item/impl_item.rs
+++ feo-ast/src/item/impl_item.rs
@@ -1,12 +1,14 @@
+use feo_types::span::{Span, Spanned};
+
 use crate::{
-    expression::InnerAttr, keyword::KeywordKind, path::SimplePath, ty::Type, type_utils::Brace,
+    expression::InnerAttr, keyword::Keyword, path::SimplePath, ty::Type, type_utils::Brace,
 };
 
 use super::{AssociatedItem, ImplItem, Item, VisibilityKind, WhereClause};
 
 pub struct InherentImpl<T> {
     visibility_opt: Option<VisibilityKind>,
-    kw_impl: KeywordKind,
+    kw_impl: Keyword,
     object_type: Box<dyn Type>,
     where_clause_opt: Option<WhereClause>,
     open_brace: Brace,
@@ -19,12 +21,29 @@ impl<T> Item for InherentImpl<T> {}
 
 impl<T, I> ImplItem<I> for InherentImpl<T> where I: Item {}
 
+impl<T> Spanned for InherentImpl<T> {
+    fn span(&self) -> Span {
+        let start_pos = if let Some(v) = &self.visibility_opt {
+            v.span().start()
+        } else {
+            self.kw_impl.span().start()
+        };
+
+        let end_pos = self.close_brace.span().end();
+        let source = self.kw_impl.span().source();
+
+        let span = Span::new(source.as_str(), start_pos, end_pos);
+
+        span
+    }
+}
+
 pub struct TraitImpl<T> {
     visibility_opt: Option<VisibilityKind>,
-    kw_unsafe_opt: Option<KeywordKind>,
-    kw_impl: KeywordKind,
+    kw_unsafe_opt: Option<Keyword>,
+    kw_impl: Keyword,
     trait_path: SimplePath,
-    kw_for: KeywordKind,
+    kw_for: Keyword,
     object_type: Box<dyn Type>,
     where_clause_opt: Option<WhereClause>,
     open_brace: Brace,
