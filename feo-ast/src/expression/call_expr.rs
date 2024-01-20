cde253588650e0615214927dbb87ca70ba1c1465
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-20 16:02:34 +0200

    implement Pattern and Spanned for CallParams

--- feo-ast/src/expression/call_expr.rs
+++ feo-ast/src/expression/call_expr.rs
@@ -40,7 +40,26 @@ pub struct CallParams {
     trailing_comma_opt: Option<Comma>,
 }
 
-// impl Pattern for CallParams {}
+impl Pattern for CallParams {}
+
+impl Spanned for CallParams {
+    fn span(&self) -> Span {
+        let start_pos = self.first_param.span().start();
+        let end_pos = match self.subsequent_params.last() {
+            Some(s) => match &self.trailing_comma_opt {
+                Some(t) => t.span().end(),
+                None => s.1.span().end(),
+            },
+            None => self.first_param.span().end(),
+        };
+
+        let source = self.first_param.span().source();
+
+        let span = Span::new(source.as_str(), start_pos, end_pos);
+
+        span
+    }
+}
 
 pub struct MethodCallExpr {
     object: Box<dyn Expression>,
