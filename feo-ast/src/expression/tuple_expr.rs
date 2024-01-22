f6ddfbe8b4cccc17c4c5f1896fb9b814114d580f
Matthew Pratt <matthew.pratt13@gmail.com>
2024-01-22 18:27:51 +0200

    change field type

--- feo-ast/src/expression/tuple_expr.rs
+++ feo-ast/src/expression/tuple_expr.rs
@@ -1,7 +1,9 @@
-use feo_types::span::{Span, Spanned};
+use feo_types::{
+    span::{Span, Spanned},
+    Literal,
+};
 
 use crate::{
-    literals::UIntLiteral,
     statement::Statement,
     type_utils::{Comma, Dot, Parenthesis},
 };
@@ -40,16 +42,16 @@ pub struct TupleElements {
 }
 
 pub struct TupleIndexingExpr {
-    object: Box<dyn Expression>,
+    operand: Box<dyn Expression>,
     dot: Dot,
-    index: UIntLiteral,
+    index: Literal<u64>,
 }
 
 impl Spanned for TupleIndexingExpr {
     fn span(&self) -> Span {
-        let start_pos = self.object.span().start();
+        let start_pos = self.operand.span().start();
         let end_pos = self.dot.span().end() + format!("{:?}", self.index).len();
-        let source = self.object.span().source();
+        let source = self.operand.span().source();
 
         let span = Span::new(source.as_str(), start_pos, end_pos);
 
