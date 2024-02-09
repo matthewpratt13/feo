44dcdbbdd92e75779af914a4eb5e4d574b1f54bf
Matthew Pratt <matthew.pratt13@gmail.com>
2024-02-09 18:13:03 +0200

    remove commented code

--- feo-types/src/primitive.rs
+++ feo-types/src/primitive.rs
@@ -2,38 +2,6 @@ use std::fmt::Debug;
 
 use crate::U256;
 
-// pub trait PrimitiveType
-// where
-//     Self: Sized + Debug + Clone + PartialEq + 'static,
-// {
-// }
-
-// impl PrimitiveType for char {}
-
-// impl PrimitiveType for &'static str {}
-
-// impl PrimitiveType for String {}
-
-// impl PrimitiveType for bool {}
-
-// impl PrimitiveType for i32 {}
-
-// impl PrimitiveType for i64 {}
-
-// impl PrimitiveType for u8 {}
-
-// impl PrimitiveType for u16 {}
-
-// impl PrimitiveType for u32 {}
-
-// impl PrimitiveType for u64 {}
-
-// impl PrimitiveType for U256 {}
-
-// impl PrimitiveType for f32 {}
-
-// impl PrimitiveType for f64 {}
-
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Primitive {
     Char(char),
@@ -49,28 +17,3 @@ pub enum Primitive {
     F32(f32),
     F64(f64),
 }
-
-// #[derive(Debug, Clone)]
-// pub struct Primitive<P: PrimitiveType>(P);
-
-// impl<P> Primitive<P>
-// where
-//     P: PrimitiveType,
-// {
-//     pub fn new(raw_value: P) -> Primitive<P> {
-//         Primitive(raw_value)
-//     }
-
-//     pub fn raw_value(self) -> P {
-//         self.0
-//     }
-// }
-
-// impl<P> Spanned for Primitive<P>
-// where
-//     P: PrimitiveType,
-// {
-//     fn span(&self) -> Span {
-//         Span::default()
-//     }
-// }
