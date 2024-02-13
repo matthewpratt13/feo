4e2787644e3e2c8d74685284341d218687af13df
Matthew Pratt <matthew.pratt13@gmail.com>
2024-02-13 19:11:49 +0200

    complete TODO

--- feo-parser/src/expression/literal_expr.rs
+++ feo-parser/src/expression/literal_expr.rs
@@ -1,10 +1,8 @@
 use feo_ast::token::Token;
 use feo_error::{handler::ErrorEmitted, parser_error::ParserErrorKind};
-use feo_types::literal::LiteralKind;
+use feo_types::literal::{FloatType, IntType, LiteralKind, UIntType};
 
 use crate::{parse::Parse, parser::Parser};
-
-// TODO: match `IntType` etc. variants
 
 impl Parse for LiteralKind {
     fn parse(parser: &mut Parser) -> Result<Option<Self>, ErrorEmitted>
@@ -15,10 +13,31 @@ impl Parse for LiteralKind {
             Token::CharLit(c) => Ok(Some(LiteralKind::Char(c))),
             Token::StringLit(s) => Ok(Some(LiteralKind::String(s))),
             Token::BoolLit(b) => Ok(Some(LiteralKind::Bool(b))),
-            Token::IntLit(i) => Ok(Some(LiteralKind::I64(i))),
-            Token::UIntLit(ui) => Ok(Some(LiteralKind::U64(ui))),
+            Token::IntLit(i) => match i.clone().into_inner() {
+                Some(t) => match t {
+                    IntType::I32(_) => Ok(Some(LiteralKind::I32(i))),
+                    IntType::I64(_) => Ok(Some(LiteralKind::I64(i))),
+                },
+                None => Err(parser.log_error(ParserErrorKind::InvalidToken)),
+            },
+            Token::UIntLit(ui) => match ui.clone().into_inner() {
+                Some(t) => match t {
+                    UIntType::U8(_) => Ok(Some(LiteralKind::U8(ui))),
+                    UIntType::U16(_) => Ok(Some(LiteralKind::U16(ui))),
+                    UIntType::U32(_) => Ok(Some(LiteralKind::U32(ui))),
+                    UIntType::U64(_) => Ok(Some(LiteralKind::U64(ui))),
+                },
+                None => Err(parser.log_error(ParserErrorKind::InvalidToken)),
+            },
+            // Token::UIntLit(ui) => Ok(Some(LiteralKind::U64(ui))),
             Token::U256Lit(u) => Ok(Some(LiteralKind::U256(u))),
-            Token::FloatLit(f) => Ok(Some(LiteralKind::F64(f))),
+            Token::FloatLit(f) => match f.clone().into_inner() {
+                Some(t) => match t {
+                    FloatType::F32(_) => Ok(Some(LiteralKind::F32(f))),
+                    FloatType::F64(_) => Ok(Some(LiteralKind::F64(f))),
+                },
+                None => Err(parser.log_error(ParserErrorKind::InvalidToken)),
+            },
             _ => Err(parser.log_error(ParserErrorKind::UnexpectedToken)),
         }
     }
