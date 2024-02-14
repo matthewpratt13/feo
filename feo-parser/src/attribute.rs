375a0e944b1f740fd2ab4f08acf0de058ed4e639
Matthew Pratt <matthew.pratt13@gmail.com>
2024-02-14 12:59:29 +0200

    implement Peek for AttributeKind

--- feo-parser/src/attribute.rs
+++ feo-parser/src/attribute.rs
@@ -1,1 +1,27 @@
+use feo_ast::{expression::AttributeKind, path::SimplePathSegmentKind};
+use feo_types::keyword::KeywordKind;
 
+use crate::{parse::Peek, parser::Peeker};
+
+impl Peek for AttributeKind {
+    fn peek(peeker: Peeker<'_>) -> Option<Self>
+    where
+        Self: Sized,
+    {
+        let attr_kind = if let Ok(k) = peeker.peek_keyword() {
+            match k.keyword_kind {
+                KeywordKind::KwAbstract => AttributeKind::KwAbstract(k),
+                KeywordKind::KwExport => AttributeKind::KwExport(k),
+                KeywordKind::KwExtern => AttributeKind::KwExtern(k),
+                KeywordKind::KwUnsafe => AttributeKind::KwUnsafe(k),
+                _ => return None,
+            }
+        } else if let Some(p) = SimplePathSegmentKind::peek(peeker) {
+            AttributeKind::Path(p)
+        } else {
+            return None;
+        };
+
+        Some(attr_kind)
+    }
+}
