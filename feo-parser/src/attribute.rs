8d2ad8e234ff03b7170b5acdc4aa3286d34d7947
Matthew Pratt <matthew.pratt13@gmail.com>
2024-02-23 16:20:00 +0200

    add OuterAttr test

--- feo-parser/src/attribute.rs
+++ feo-parser/src/attribute.rs
@@ -172,3 +172,29 @@ impl ParseTerm for OuterAttr {
         Err(parser.errors())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use feo_error::handler::Handler;
+
+    use crate::lexer::Lexer;
+
+    use super::*;
+
+    #[test]
+    fn parse_attribute() {
+        let source_code = r#"#[unsafe]"#;
+
+        let handler = Handler::default();
+        let mut lexer = Lexer::new(&source_code, handler.clone());
+        let token_stream = lexer.lex().expect("unable to lex source code");
+
+        println!("{:#?}", token_stream);
+
+        let mut parser = Parser::new(token_stream, handler);
+
+        let outer_attr = OuterAttr::parse(&mut parser).expect("unable to parse outer attribute");
+
+        println!("{:#?}", outer_attr);
+    }
+}
