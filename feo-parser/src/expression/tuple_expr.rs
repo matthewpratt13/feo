9ad610308dd270c712b698037b15716d3da1f6a5
Matthew Pratt <matthew.pratt13@gmail.com>
2024-02-29 17:42:42 +0200

    implement ParseTerm for TupleElements

--- feo-parser/src/expression/tuple_expr.rs
+++ feo-parser/src/expression/tuple_expr.rs
@@ -1,5 +1,9 @@
-use feo_ast::expression::{TupleElements, TupleExpr, TupleIndexExpr};
-use feo_error::error::CompilerError;
+use feo_ast::{
+    expression::{Returnable, TupleElements, TupleExpr, TupleIndexExpr},
+    token::Token,
+};
+use feo_error::{error::CompilerError, parser_error::ParserErrorKind};
+use feo_types::{punctuation::PuncKind, utils::Comma, Punctuation};
 
 use crate::{
     parse::{ParseExpr, ParseTerm},
@@ -18,7 +22,56 @@ impl ParseTerm for TupleElements {
     where
         Self: Sized,
     {
-        todo!()
+        if let Some(first_element) = Returnable::parse(parser)? {
+            let mut subsequent_elements: Vec<(Comma, Returnable)> = Vec::new();
+
+            let mut comma_opt = parser.peek_current::<Punctuation>();
+
+            while let Some(Punctuation {
+                punc_kind: PuncKind::Comma,
+                ..
+            }) = comma_opt
+            {
+                parser.next_token();
+
+                if let Some(next_element) = Returnable::parse(parser)? {
+                    subsequent_elements.push((comma_opt.unwrap(), next_element));
+
+                    if let Some(p) = parser.peek_next::<Punctuation>() {
+                        comma_opt = Some(p);
+                        parser.next_token();
+                    } else {
+                        break;
+                    }
+                } else {
+                    parser.log_error(ParserErrorKind::UnexpectedToken {
+                        expected: "`Returnable`".to_string(),
+                        found: parser.current_token().unwrap_or(Token::EOF).to_string(),
+                    });
+                    break;
+                }
+            }
+
+            let trailing_comma_opt = parser.peek_current::<Punctuation>();
+
+            parser.next_token();
+
+            if !subsequent_elements.is_empty() {
+                return Ok(Some(TupleElements {
+                    first_element: Box::new(first_element),
+                    subsequent_elements_opt: Some(subsequent_elements),
+                    trailing_comma_opt,
+                }));
+            }
+
+            return Ok(Some(TupleElements {
+                first_element: Box::new(first_element),
+                subsequent_elements_opt: None,
+                trailing_comma_opt,
+            }));
+        }
+
+        Err(parser.errors())
     }
 }
 
