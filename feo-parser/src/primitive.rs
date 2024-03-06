53430450981d174cd576a21a389ab03f84e1adb7
Matthew Pratt <matthew.pratt13@gmail.com>
2024-03-06 19:03:06 +0200

    implement ParseTern for the remaining Primitive types

--- feo-parser/src/primitive.rs
+++ feo-parser/src/primitive.rs
@@ -1,5 +1,9 @@
 use feo_error::error::CompilerError;
-use feo_types::{literal::LiteralKind, primitive::Primitive};
+use feo_types::{
+    literal::{FloatType, IntType, LiteralKind, UIntType},
+    primitive::Primitive,
+    U256,
+};
 
 use crate::{parse::ParseTerm, parser::Parser};
 
@@ -49,6 +53,192 @@ impl ParseTerm for Primitive<bool> {
 
             match l {
                 LiteralKind::Bool(b) => Ok(Some(Primitive(b.into_inner().unwrap()))),
+                _ => Ok(None),
+            }
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl ParseTerm for Primitive<i32> {
+    fn parse(parser: &mut Parser) -> Result<Option<Self>, Vec<CompilerError>>
+    where
+        Self: Sized,
+    {
+        if let Some(l) = parser.peek_current::<LiteralKind>() {
+            parser.next_token();
+
+            match l {
+                LiteralKind::Int(i) => match i.into_inner().unwrap() {
+                    IntType::I32(s) => Ok(Some(Primitive(s))),
+                    _ => Ok(None),
+                },
+                _ => Ok(None),
+            }
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl ParseTerm for Primitive<i64> {
+    fn parse(parser: &mut Parser) -> Result<Option<Self>, Vec<CompilerError>>
+    where
+        Self: Sized,
+    {
+        if let Some(l) = parser.peek_current::<LiteralKind>() {
+            parser.next_token();
+
+            match l {
+                LiteralKind::Int(i) => match i.into_inner().unwrap() {
+                    IntType::I64(s) => Ok(Some(Primitive(s))),
+                    _ => Ok(None),
+                },
+                _ => Ok(None),
+            }
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl ParseTerm for Primitive<u8> {
+    fn parse(parser: &mut Parser) -> Result<Option<Self>, Vec<CompilerError>>
+    where
+        Self: Sized,
+    {
+        if let Some(l) = parser.peek_current::<LiteralKind>() {
+            parser.next_token();
+
+            match l {
+                LiteralKind::UInt(ui) => match ui.into_inner().unwrap() {
+                    UIntType::U8(s) => Ok(Some(Primitive(s))),
+                    _ => Ok(None),
+                },
+                _ => Ok(None),
+            }
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl ParseTerm for Primitive<u16> {
+    fn parse(parser: &mut Parser) -> Result<Option<Self>, Vec<CompilerError>>
+    where
+        Self: Sized,
+    {
+        if let Some(l) = parser.peek_current::<LiteralKind>() {
+            parser.next_token();
+
+            match l {
+                LiteralKind::UInt(ui) => match ui.into_inner().unwrap() {
+                    UIntType::U16(s) => Ok(Some(Primitive(s))),
+                    _ => Ok(None),
+                },
+                _ => Ok(None),
+            }
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl ParseTerm for Primitive<u32> {
+    fn parse(parser: &mut Parser) -> Result<Option<Self>, Vec<CompilerError>>
+    where
+        Self: Sized,
+    {
+        if let Some(l) = parser.peek_current::<LiteralKind>() {
+            parser.next_token();
+
+            match l {
+                LiteralKind::UInt(ui) => match ui.into_inner().unwrap() {
+                    UIntType::U32(s) => Ok(Some(Primitive(s))),
+                    _ => Ok(None),
+                },
+                _ => Ok(None),
+            }
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl ParseTerm for Primitive<u64> {
+    fn parse(parser: &mut Parser) -> Result<Option<Self>, Vec<CompilerError>>
+    where
+        Self: Sized,
+    {
+        if let Some(l) = parser.peek_current::<LiteralKind>() {
+            parser.next_token();
+
+            match l {
+                LiteralKind::UInt(ui) => match ui.into_inner().unwrap() {
+                    UIntType::U64(s) => Ok(Some(Primitive(s))),
+                    _ => Ok(None),
+                },
+                _ => Ok(None),
+            }
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl ParseTerm for Primitive<U256> {
+    fn parse(parser: &mut Parser) -> Result<Option<Self>, Vec<CompilerError>>
+    where
+        Self: Sized,
+    {
+        if let Some(l) = parser.peek_current::<LiteralKind>() {
+            parser.next_token();
+
+            match l {
+                LiteralKind::U256(u) => Ok(Some(Primitive(u.into_inner().unwrap()))),
+                _ => Ok(None),
+            }
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl ParseTerm for Primitive<f32> {
+    fn parse(parser: &mut Parser) -> Result<Option<Self>, Vec<CompilerError>>
+    where
+        Self: Sized,
+    {
+        if let Some(l) = parser.peek_current::<LiteralKind>() {
+            parser.next_token();
+
+            match l {
+                LiteralKind::Float(f) => match f.into_inner().unwrap() {
+                    FloatType::F32(s) => Ok(Some(Primitive(s))),
+                    _ => Ok(None),
+                },
+                _ => Ok(None),
+            }
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl ParseTerm for Primitive<f64> {
+    fn parse(parser: &mut Parser) -> Result<Option<Self>, Vec<CompilerError>>
+    where
+        Self: Sized,
+    {
+        if let Some(l) = parser.peek_current::<LiteralKind>() {
+            parser.next_token();
+
+            match l {
+                LiteralKind::Float(f) => match f.into_inner().unwrap() {
+                    FloatType::F64(s) => Ok(Some(Primitive(s))),
+                    _ => Ok(None),
+                },
                 _ => Ok(None),
             }
         } else {
