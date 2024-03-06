4139f6b7206d7667f53c7df562f668f512c4c311
Matthew Pratt <matthew.pratt13@gmail.com>
2024-03-06 18:52:31 +0200

    implement ParseTerm for char and bool

--- feo-parser/src/primitive.rs
+++ feo-parser/src/primitive.rs
@@ -3,6 +3,24 @@ use feo_types::{literal::LiteralKind, primitive::Primitive};
 
 use crate::{parse::ParseTerm, parser::Parser};
 
+impl ParseTerm for Primitive<char> {
+    fn parse(parser: &mut Parser) -> Result<Option<Self>, Vec<CompilerError>>
+    where
+        Self: Sized,
+    {
+        if let Some(l) = parser.peek_current::<LiteralKind>() {
+            parser.next_token();
+
+            match l {
+                LiteralKind::Char(c) => Ok(Some(Primitive(c.into_inner().unwrap()))),
+                _ => Ok(None),
+            }
+        } else {
+            Ok(None)
+        }
+    }
+}
+
 impl ParseTerm for Primitive<String> {
     fn parse(parser: &mut Parser) -> Result<Option<Self>, Vec<CompilerError>>
     where
@@ -10,9 +28,27 @@ impl ParseTerm for Primitive<String> {
     {
         if let Some(l) = parser.peek_current::<LiteralKind>() {
             parser.next_token();
-            
+
             match l {
                 LiteralKind::String(s) => Ok(Some(Primitive(s.into_inner().unwrap()))),
+                _ => Ok(None),
+            }
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl ParseTerm for Primitive<bool> {
+    fn parse(parser: &mut Parser) -> Result<Option<Self>, Vec<CompilerError>>
+    where
+        Self: Sized,
+    {
+        if let Some(l) = parser.peek_current::<LiteralKind>() {
+            parser.next_token();
+
+            match l {
+                LiteralKind::Bool(b) => Ok(Some(Primitive(b.into_inner().unwrap()))),
                 _ => Ok(None),
             }
         } else {
