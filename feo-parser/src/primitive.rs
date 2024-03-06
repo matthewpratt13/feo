8e6c696960a4a366d10f61e648156e205b7b32b3
Matthew Pratt <matthew.pratt13@gmail.com>
2024-03-06 19:14:24 +0200

    small change

--- feo-parser/src/primitive.rs
+++ feo-parser/src/primitive.rs
@@ -13,10 +13,11 @@ impl ParseTerm for Primitive<char> {
         Self: Sized,
     {
         if let Some(l) = parser.peek_current::<LiteralKind>() {
-            parser.next_token();
-
             match l {
-                LiteralKind::Char(c) => Ok(Some(Primitive(c.into_inner().unwrap()))),
+                LiteralKind::Char(c) => {
+                    parser.next_token();
+                    Ok(Some(Primitive(c.into_inner().unwrap())))
+                }
                 _ => Ok(None),
             }
         } else {
@@ -31,10 +32,11 @@ impl ParseTerm for Primitive<String> {
         Self: Sized,
     {
         if let Some(l) = parser.peek_current::<LiteralKind>() {
-            parser.next_token();
-
             match l {
-                LiteralKind::String(s) => Ok(Some(Primitive(s.into_inner().unwrap()))),
+                LiteralKind::String(s) => {
+                    parser.next_token();
+                    Ok(Some(Primitive(s.into_inner().unwrap())))
+                }
                 _ => Ok(None),
             }
         } else {
@@ -49,10 +51,11 @@ impl ParseTerm for Primitive<bool> {
         Self: Sized,
     {
         if let Some(l) = parser.peek_current::<LiteralKind>() {
-            parser.next_token();
-
             match l {
-                LiteralKind::Bool(b) => Ok(Some(Primitive(b.into_inner().unwrap()))),
+                LiteralKind::Bool(b) => {
+                    parser.next_token();
+                    Ok(Some(Primitive(b.into_inner().unwrap())))
+                }
                 _ => Ok(None),
             }
         } else {
@@ -67,11 +70,12 @@ impl ParseTerm for Primitive<i32> {
         Self: Sized,
     {
         if let Some(l) = parser.peek_current::<LiteralKind>() {
-            parser.next_token();
-
             match l {
                 LiteralKind::Int(i) => match i.into_inner().unwrap() {
-                    IntType::I32(s) => Ok(Some(Primitive(s))),
+                    IntType::I32(s) => {
+                        parser.next_token();
+                        Ok(Some(Primitive(s)))
+                    }
                     _ => Ok(None),
                 },
                 _ => Ok(None),
@@ -88,11 +92,12 @@ impl ParseTerm for Primitive<i64> {
         Self: Sized,
     {
         if let Some(l) = parser.peek_current::<LiteralKind>() {
-            parser.next_token();
-
             match l {
                 LiteralKind::Int(i) => match i.into_inner().unwrap() {
-                    IntType::I64(s) => Ok(Some(Primitive(s))),
+                    IntType::I64(s) => {
+                        parser.next_token();
+                        Ok(Some(Primitive(s)))
+                    }
                     _ => Ok(None),
                 },
                 _ => Ok(None),
@@ -109,11 +114,12 @@ impl ParseTerm for Primitive<u8> {
         Self: Sized,
     {
         if let Some(l) = parser.peek_current::<LiteralKind>() {
-            parser.next_token();
-
             match l {
                 LiteralKind::UInt(ui) => match ui.into_inner().unwrap() {
-                    UIntType::U8(s) => Ok(Some(Primitive(s))),
+                    UIntType::U8(s) => {
+                        parser.next_token();
+                        Ok(Some(Primitive(s)))
+                    }
                     _ => Ok(None),
                 },
                 _ => Ok(None),
@@ -130,11 +136,12 @@ impl ParseTerm for Primitive<u16> {
         Self: Sized,
     {
         if let Some(l) = parser.peek_current::<LiteralKind>() {
-            parser.next_token();
-
             match l {
                 LiteralKind::UInt(ui) => match ui.into_inner().unwrap() {
-                    UIntType::U16(s) => Ok(Some(Primitive(s))),
+                    UIntType::U16(s) => {
+                        parser.next_token();
+                        Ok(Some(Primitive(s)))
+                    }
                     _ => Ok(None),
                 },
                 _ => Ok(None),
@@ -151,11 +158,12 @@ impl ParseTerm for Primitive<u32> {
         Self: Sized,
     {
         if let Some(l) = parser.peek_current::<LiteralKind>() {
-            parser.next_token();
-
             match l {
                 LiteralKind::UInt(ui) => match ui.into_inner().unwrap() {
-                    UIntType::U32(s) => Ok(Some(Primitive(s))),
+                    UIntType::U32(s) => {
+                        parser.next_token();
+                        Ok(Some(Primitive(s)))
+                    }
                     _ => Ok(None),
                 },
                 _ => Ok(None),
@@ -172,11 +180,12 @@ impl ParseTerm for Primitive<u64> {
         Self: Sized,
     {
         if let Some(l) = parser.peek_current::<LiteralKind>() {
-            parser.next_token();
-
             match l {
                 LiteralKind::UInt(ui) => match ui.into_inner().unwrap() {
-                    UIntType::U64(s) => Ok(Some(Primitive(s))),
+                    UIntType::U64(s) => {
+                        parser.next_token();
+                        Ok(Some(Primitive(s)))
+                    }
                     _ => Ok(None),
                 },
                 _ => Ok(None),
@@ -193,10 +202,11 @@ impl ParseTerm for Primitive<U256> {
         Self: Sized,
     {
         if let Some(l) = parser.peek_current::<LiteralKind>() {
-            parser.next_token();
-
             match l {
-                LiteralKind::U256(u) => Ok(Some(Primitive(u.into_inner().unwrap()))),
+                LiteralKind::U256(u) => {
+                    parser.next_token();
+                    Ok(Some(Primitive(u.into_inner().unwrap())))
+                }
                 _ => Ok(None),
             }
         } else {
@@ -211,11 +221,12 @@ impl ParseTerm for Primitive<f32> {
         Self: Sized,
     {
         if let Some(l) = parser.peek_current::<LiteralKind>() {
-            parser.next_token();
-
             match l {
                 LiteralKind::Float(f) => match f.into_inner().unwrap() {
-                    FloatType::F32(s) => Ok(Some(Primitive(s))),
+                    FloatType::F32(s) => {
+                        parser.next_token();
+                        Ok(Some(Primitive(s)))
+                    }
                     _ => Ok(None),
                 },
                 _ => Ok(None),
@@ -232,11 +243,12 @@ impl ParseTerm for Primitive<f64> {
         Self: Sized,
     {
         if let Some(l) = parser.peek_current::<LiteralKind>() {
-            parser.next_token();
-
             match l {
                 LiteralKind::Float(f) => match f.into_inner().unwrap() {
-                    FloatType::F64(s) => Ok(Some(Primitive(s))),
+                    FloatType::F64(s) => {
+                        parser.next_token();
+                        Ok(Some(Primitive(s)))
+                    }
                     _ => Ok(None),
                 },
                 _ => Ok(None),
