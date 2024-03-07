4e0be47dba9cee28370252e8b962651a990e0030
Matthew Pratt <matthew.pratt13@gmail.com>
2024-03-07 10:19:20 +0200

    add test prep

--- feo-parser/src/ty/array_type.rs
+++ feo-parser/src/ty/array_type.rs
@@ -83,3 +83,32 @@ impl ParseTerm for ArrayType {
         Err(parser.errors())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use feo_error::handler::Handler;
+
+    use crate::lexer::Lexer;
+
+    use super::*;
+
+    #[ignore] // TODO: remove when testing
+    #[test]
+    fn parse_array_expr() {
+        let source_code = r#"[u8; 32]"#;
+
+        let handler = Handler::default();
+
+        let mut lexer = Lexer::new(&source_code, handler.clone());
+
+        let token_stream = lexer.lex().expect("unable to lex source code");
+
+        // println!("{:#?}", token_stream);
+
+        let mut parser = Parser::new(token_stream, handler);
+
+        let array_type = ArrayType::parse(&mut parser).expect("unable to parse array type");
+
+        println!("{:#?}", array_type);
+    }
+}
