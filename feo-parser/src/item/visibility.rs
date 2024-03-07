2e43583f2a90d294e60fefa164f53ef89831c06d
Matthew Pratt <matthew.pratt13@gmail.com>
2024-03-07 21:12:05 +0200

    test VisibilityKind

--- feo-parser/src/item/visibility.rs
+++ feo-parser/src/item/visibility.rs
@@ -113,3 +113,34 @@ impl ParseTerm for PubCrateVisibility {
         Err(parser.errors())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use feo_error::handler::Handler;
+
+    use crate::lexer::Lexer;
+
+    use super::*;
+
+    #[test]
+    fn parse_visibility_kind() {
+        let source_code = r#"
+        pub
+        pub(crate)"#;
+
+        let handler = Handler::default();
+
+        let mut lexer = Lexer::new(&source_code, handler.clone());
+
+        let token_stream = lexer.lex().expect("unable to lex source code");
+
+        // println!("{:#?}", token_stream);
+
+        let mut parser = Parser::new(token_stream, handler);
+
+        let visibility_kind =
+            VisibilityKind::parse(&mut parser).expect("unable to parse visibility");
+
+        println!("{:#?}", visibility_kind);
+    }
+}
