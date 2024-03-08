53e862d85ed74e213df2b0c98fc5e5867a4ae1e0
Matthew Pratt <matthew.pratt13@gmail.com>
2024-03-08 20:02:41 +0200

    imple ParseTerm for IdentifierPatt

--- feo-ast/src/pattern/identifier_patt.rs
+++ feo-ast/src/pattern/identifier_patt.rs
@@ -6,9 +6,9 @@ use feo_types::{
 
 #[derive(Debug, Clone)]
 pub struct IdentifierPatt {
-    kw_ref_opt: Option<KwRef>,
-    kw_mut_opt: Option<KwMut>,
-    name: Identifier,
+    pub kw_ref_opt: Option<KwRef>,
+    pub kw_mut_opt: Option<KwMut>,
+    pub name: Identifier,
 }
 
 impl Spanned for IdentifierPatt {
--- feo-parser/src/pattern/identifier_patt.rs
+++ feo-parser/src/pattern/identifier_patt.rs
@@ -1,5 +1,6 @@
 use feo_ast::pattern::IdentifierPatt;
 use feo_error::error::CompilerError;
+use feo_types::{keyword::KeywordKind, Identifier, Keyword};
 
 use crate::{parse::ParseTerm, parser::Parser};
 
@@ -8,6 +9,36 @@ impl ParseTerm for IdentifierPatt {
     where
         Self: Sized,
     {
-        todo!()
+        let kw_ref_opt = parser.peek_current::<Keyword>();
+
+        if let Some(Keyword {
+            keyword_kind: KeywordKind::KwRef,
+            ..
+        }) = kw_ref_opt
+        {
+            parser.next_token();
+        }
+
+        let kw_mut_opt = parser.peek_current::<Keyword>();
+
+        if let Some(Keyword {
+            keyword_kind: KeywordKind::KwMut,
+            ..
+        }) = kw_mut_opt
+        {
+            parser.next_token();
+        }
+
+        if let Some(name) = parser.peek_current::<Identifier>() {
+            parser.next_token();
+
+            return Ok(Some(IdentifierPatt {
+                kw_ref_opt,
+                kw_mut_opt,
+                name,
+            }));
+        }
+
+        Ok(None)
     }
 }
