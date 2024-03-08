efc3a0adedb0085af6604884fb402b1734ecb934
Matthew Pratt <matthew.pratt13@gmail.com>
2024-03-08 20:29:35 +0200

    add test

--- feo-parser/src/item/constant_var_def.rs
+++ feo-parser/src/item/constant_var_def.rs
@@ -149,3 +149,33 @@ impl ParseTerm for ConstantVarDef {
         Err(parser.errors())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use feo_error::handler::Handler;
+
+    use crate::lexer::Lexer;
+
+    use super::*;
+
+    #[ignore] // TODO: remove when testing
+    #[test]
+    fn parse_constant_var_def() {
+        let source_code = r#"const foo: u64 = 2;"#;
+
+        let handler = Handler::default();
+
+        let mut lexer = Lexer::new(&source_code, handler.clone());
+
+        let token_stream = lexer.lex().expect("unable to lex source code");
+
+        // println!("{:#?}", token_stream);
+
+        let mut parser = Parser::new(token_stream, handler);
+
+        let constant_var_def =
+            ConstantVarDef::parse(&mut parser).expect("unable to constant var def");
+
+        println!("{:#?}", constant_var_def);
+    }
+}
