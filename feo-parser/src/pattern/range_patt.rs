9a14c3bd279e0dabbad5805355e73efd36e54e12
Matthew Pratt <matthew.pratt13@gmail.com>
2024-03-09 13:30:28 +0200

    add tests

--- feo-parser/src/pattern/range_patt.rs
+++ feo-parser/src/pattern/range_patt.rs
@@ -194,4 +194,24 @@ mod tests {
 
         println!("{:#?}", range_inclusive_patt);
     }
+
+    #[test]
+    fn parse_range_to_inclusive_patt() {
+        let source_code = r#"..=10"#;
+
+        let handler = Handler::default();
+
+        let mut lexer = Lexer::new(&source_code, handler.clone());
+
+        let token_stream = lexer.lex().expect("unable to lex source code");
+
+        println!("{:#?}", token_stream);
+
+        let mut parser = Parser::new(token_stream, handler);
+
+        let range_to_inclusive_patt =
+            RangeFromPatt::parse(&mut parser).expect("unable to parse `RangeToInclusivePatt`");
+
+        println!("{:#?}", range_to_inclusive_patt);
+    }
 }
