5b38fd09e858ec4dfb4ddaa51c9b8a4e86c85db0
Matthew Pratt <matthew.pratt13@gmail.com>
2024-03-09 13:03:53 +0200

    add test for RangeFromPatt; failing

--- feo-parser/src/pattern/range_patt.rs
+++ feo-parser/src/pattern/range_patt.rs
@@ -93,3 +93,32 @@ impl ParseTerm for RangeToInclusivePatt {
         todo!()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use feo_error::handler::Handler;
+
+    use crate::lexer::Lexer;
+
+    use super::*;
+
+    #[test]
+    fn parse_range_from_patt() {
+        let source_code = r#"1.."#;
+
+        let handler = Handler::default();
+
+        let mut lexer = Lexer::new(&source_code, handler.clone());
+
+        let token_stream = lexer.lex().expect("unable to lex source code");
+
+        // println!("{:#?}", token_stream);
+
+        let mut parser = Parser::new(token_stream, handler);
+
+        let range_from_patt =
+            RangeFromPatt::parse(&mut parser).expect("unable to parse `RangeFromPatt`");
+
+        println!("{:#?}", range_from_patt);
+    }
+}
