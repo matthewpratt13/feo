e53247dbec611c55bb0072f4f54fa933abc98be3
Matthew Pratt <matthew.pratt13@gmail.com>
2024-03-11 16:19:03 +0200

    small change

--- feo-parser/src/expression/return_expr.rs
+++ feo-parser/src/expression/return_expr.rs
@@ -1,8 +1,5 @@
-use feo_ast::{
-    expression::{ReturnExpr, Returnable},
-    token::Token,
-};
-use feo_error::{error::CompilerError, parser_error::ParserErrorKind};
+use feo_ast::expression::{ReturnExpr, Returnable};
+use feo_error::error::CompilerError;
 use feo_types::{keyword::KeywordKind, Keyword};
 
 use crate::{parse::ParseExpr, parser::Parser};
@@ -21,29 +18,20 @@ impl ParseExpr for ReturnExpr {
         {
             parser.next_token();
 
-            if let Some(expr) = Returnable::parse(parser)? {
+            let expression_opt = if let Some(e) = Returnable::parse(parser)? {
                 parser.next_token();
-
-                return Ok(Some(ReturnExpr {
-                    kw_return: kw_return_opt.unwrap(),
-                    expression_opt: Some(Box::new(expr)),
-                }));
-            }
-
-            parser.next_token();
+                Some(Box::new(e))
+            } else {
+                None
+            };
 
             return Ok(Some(ReturnExpr {
                 kw_return: kw_return_opt.unwrap(),
-                expression_opt: None,
+                expression_opt,
             }));
         }
 
-        parser.log_error(ParserErrorKind::UnexpectedToken {
-            expected: "`Returnable`".to_string(),
-            found: parser.current_token().unwrap_or(Token::EOF).to_string(),
-        });
-
-        Err(parser.errors())
+        Ok(None)
     }
 }
 
