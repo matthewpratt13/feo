df95a5527cd9a20d5c717ff04eaefd78131ce1bb
Matthew Pratt <matthew.pratt13@gmail.com>
2024-03-11 20:45:46 +0200

    implement ParseTerm for StructPattField

--- feo-parser/src/pattern/struct_patt.rs
+++ feo-parser/src/pattern/struct_patt.rs
@@ -1,7 +1,13 @@
-use feo_ast::pattern::{
-    StructPatt, StructPattField, StructPattFields, TupleStructPatt, TupleStructPattFields,
+use feo_ast::{
+    attribute::OuterAttr,
+    pattern::{
+        Pattern, StructPatt, StructPattField, StructPattFields, TupleStructPatt,
+        TupleStructPattFields,
+    },
+    token::Token,
 };
-use feo_error::error::CompilerError;
+use feo_error::{error::CompilerError, parser_error::ParserErrorKind};
+use feo_types::Identifier;
 
 use crate::{parse::ParseTerm, parser::Parser};
 
@@ -10,7 +16,36 @@ impl ParseTerm for StructPattField {
     where
         Self: Sized,
     {
-        todo!()
+        let mut attributes: Vec<OuterAttr> = Vec::new();
+
+        while let Some(oa) = OuterAttr::parse(parser)? {
+            attributes.push(oa);
+            parser.next_token();
+        }
+
+        if let Some(id) = parser.peek_current::<Identifier>() {
+            parser.next_token();
+
+            if let Some(p) = Pattern::parse(parser)? {
+                parser.next_token();
+
+                let field_contents = (id, Box::new(p));
+
+                match &attributes.is_empty() {
+                    true => return Ok(Some(StructPattField(None, field_contents))),
+                    false => return Ok(Some(StructPattField(Some(attributes), field_contents))),
+                }
+            } else {
+                parser.log_error(ParserErrorKind::UnexpectedToken {
+                    expected: "`Pattern`".to_string(),
+                    found: parser.current_token().unwrap_or(Token::EOF).to_string(),
+                });
+            }
+        } else {
+            return Ok(None);
+        }
+
+        Err(parser.errors())
     }
 }
 
