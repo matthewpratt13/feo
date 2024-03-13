79c82a73f289b07f70218676dce40d452bb059ac
Matthew Pratt <matthew.pratt13@gmail.com>
2024-03-13 17:28:41 +0200

    implement ParseType for FunctionType

--- feo-parser/src/ty/function_type.rs
+++ feo-parser/src/ty/function_type.rs
@@ -1,14 +1,101 @@
-use feo_ast::ty::{ClosureType, FunctionType};
-use feo_error::error::CompilerError;
+use feo_ast::{
+    item::FunctionParams,
+    token::Token,
+    ty::{ClosureType, FunctionType},
+    Type,
+};
+use feo_error::{error::CompilerError, parser_error::ParserErrorKind};
+use feo_types::{
+    delimiter::{DelimKind, DelimOrientation},
+    keyword::KeywordKind,
+    punctuation::PuncKind,
+    Delimiter, Keyword, Punctuation,
+};
 
-use crate::{parse::ParseType, parser::Parser};
+use crate::{
+    parse::{ParseTerm, ParseType},
+    parser::Parser,
+};
 
 impl ParseType for FunctionType {
     fn parse(parser: &mut Parser) -> Result<Option<Self>, Vec<CompilerError>>
     where
         Self: Sized,
     {
-        todo!()
+        let kw_func_opt = parser.peek_current::<Keyword>();
+
+        if let Some(Keyword {
+            keyword_kind: KeywordKind::KwFunc,
+            ..
+        }) = kw_func_opt
+        {
+            parser.next_token();
+
+            let open_parenthesis_opt = parser.peek_current::<Delimiter>();
+
+            if let Some(Delimiter {
+                delim: (DelimKind::Parenthesis, DelimOrientation::Open),
+                ..
+            }) = open_parenthesis_opt
+            {
+                parser.next_token();
+
+                let function_params_opt = if let Some(p) = FunctionParams::parse(parser)? {
+                    parser.next_token();
+                    Some(p)
+                } else {
+                    None
+                };
+
+                let close_parenthesis_opt = parser.peek_current::<Delimiter>();
+
+                if let Some(Delimiter {
+                    delim: (DelimKind::Parenthesis, DelimOrientation::Close),
+                    ..
+                }) = close_parenthesis_opt
+                {
+                    parser.next_token();
+
+                    let return_type_opt = if let Some(Punctuation {
+                        punc_kind: PuncKind::ThinArrow,
+                        ..
+                    }) = parser.peek_current::<Punctuation>()
+                    {
+                        parser.next_token();
+
+                        if let Some(ty) = Type::parse(parser)? {
+                            parser.next_token();
+                            Some(Box::new(ty))
+                        } else {
+                            None
+                        }
+                    } else {
+                        None
+                    };
+
+                    return Ok(Some(FunctionType {
+                        kw_func: kw_func_opt.unwrap(),
+                        open_parenthesis: open_parenthesis_opt.unwrap(),
+                        function_params_opt,
+                        close_parenthesis: close_parenthesis_opt.unwrap(),
+                        return_type_opt,
+                    }));
+                } else {
+                    parser.log_error(ParserErrorKind::MissingDelimiter {
+                        delim: ")".to_string(),
+                    });
+                }
+            } else {
+                parser.log_error(ParserErrorKind::UnexpectedToken {
+                    expected: "`(`".to_string(),
+                    found: parser.current_token().unwrap_or(Token::EOF).to_string(),
+                });
+            }
+        } else {
+            return Ok(None);
+        }
+
+        Err(parser.errors())
     }
 }
 
