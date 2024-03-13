791c2fdab2d9488b651ee6a21204bc648ba32997
Matthew Pratt <matthew.pratt13@gmail.com>
2024-03-13 16:03:50 +0200

    add ParseTerm implementation sigs

--- feo-parser/src/expression/closure_expr.rs
+++ feo-parser/src/expression/closure_expr.rs
@@ -1,9 +1,32 @@
-use feo_ast::expression::{ClosureWithBlock, ClosureWithoutBlock};
+use feo_ast::expression::{
+    ClosureParam, ClosureParams, ClosureParamsOpt, ClosureWithBlock, ClosureWithoutBlock,
+};
 use feo_error::error::CompilerError;
 
-use crate::{parse::ParseExpr, parser::Parser};
+use crate::{
+    parse::{ParseExpr, ParseTerm},
+    parser::Parser,
+};
 
-impl ParseExpr for ClosureWithBlock {
+impl ParseTerm for ClosureParamsOpt {
+    fn parse(parser: &mut Parser) -> Result<Option<Self>, Vec<CompilerError>>
+    where
+        Self: Sized,
+    {
+        todo!()
+    }
+}
+
+impl ParseTerm for ClosureParam {
+    fn parse(parser: &mut Parser) -> Result<Option<Self>, Vec<CompilerError>>
+    where
+        Self: Sized,
+    {
+        todo!()
+    }
+}
+
+impl ParseTerm for ClosureParams {
     fn parse(parser: &mut Parser) -> Result<Option<Self>, Vec<CompilerError>>
     where
         Self: Sized,
@@ -13,6 +36,15 @@ impl ParseExpr for ClosureWithBlock {
 }
 
 impl ParseExpr for ClosureWithoutBlock {
+    fn parse(parser: &mut Parser) -> Result<Option<Self>, Vec<CompilerError>>
+    where
+        Self: Sized,
+    {
+        todo!()
+    }
+}
+
+impl ParseExpr for ClosureWithBlock {
     fn parse(parser: &mut Parser) -> Result<Option<Self>, Vec<CompilerError>>
     where
         Self: Sized,
