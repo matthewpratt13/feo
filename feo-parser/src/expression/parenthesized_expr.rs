6888222d03f328d29599956e06dcbf4525495bd7
Matthew Pratt <matthew.pratt13@gmail.com>
2024-03-16 16:52:43 +0200

    implement ParseExpr for ParenthesizedExpr

--- feo-parser/src/expression/parenthesized_expr.rs
+++ feo-parser/src/expression/parenthesized_expr.rs
@@ -1,13 +1,60 @@
-use feo_ast::expression::ParenthesizedExpr;
-use feo_error::error::CompilerError;
+use feo_ast::{
+    expression::{Expression, ParenthesizedExpr},
+    token::Token,
+};
+use feo_error::{error::CompilerError, parser_error::ParserErrorKind};
+use feo_types::{
+    delimiter::{DelimKind, DelimOrientation},
+    Delimiter,
+};
 
-use crate::{parse::ParseTerm, parser::Parser};
+use crate::{
+    parse::{ParseExpr, ParseTerm},
+    parser::Parser,
+};
 
 impl ParseTerm for ParenthesizedExpr {
     fn parse(parser: &mut Parser) -> Result<Option<Self>, Vec<CompilerError>>
     where
         Self: Sized,
     {
-        todo!()
+        let open_parenthesis_opt = parser.peek_current::<Delimiter>();
+
+        if let Some(Delimiter {
+            delim: (DelimKind::Parenthesis, DelimOrientation::Open),
+            ..
+        }) = open_parenthesis_opt
+        {
+            parser.next_token();
+
+            if let Some(enclosed_operand) = Expression::parse(parser)? {
+                parser.next_token();
+
+                let close_parenthesis_opt = parser.peek_current::<Delimiter>();
+
+                if let Some(Delimiter {
+                    delim: (DelimKind::Parenthesis, DelimOrientation::Close),
+                    ..
+                }) = close_parenthesis_opt
+                {
+                    parser.next_token();
+
+                    return Ok(Some(ParenthesizedExpr {
+                        open_parenthesis: open_parenthesis_opt.unwrap(),
+                        enclosed_operand: Box::new(enclosed_operand),
+                        close_parenthesis: close_parenthesis_opt.unwrap(),
+                    }));
+                }
+            } else {
+                parser.log_error(ParserErrorKind::UnexpectedToken {
+                    expected: "`Expression`".to_string(),
+                    found: parser.current_token().unwrap_or(Token::EOF).to_string(),
+                });
+            }
+        } else {
+            return Ok(None);
+        }
+
+        Err(parser.errors())
     }
 }
